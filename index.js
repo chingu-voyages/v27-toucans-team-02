537bd7ca9fa525dedf7eaaaca21597abc53ea35f
velaztro <68763480+velaztro@users.noreply.github.com>
2021-03-11 21:07:07 -0800

    -Results with format. -Clear modify. -Not calculate if empty.

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
-// waits for the DOM to load
-
-    
-//    let $submitButton = document.getElementById("submit-btn")
-
-//    $submitButton.addEventListener("click", function(e){
-//     e.preventDefault()
-//     let salesTax = $('#sales-tax').val()
-//     let downPayment = $('#down-payment').val()    
-//     let tradeIn = $('#trade-value').val()    
-//     let loanTerm = $('#loan-term').val()    
-//     let interestRate = $('#interest-rate').val()    
-
-//     console.log(salesTax, downPayment, tradeIn, loanTerm, interestRate)
-// })
-
-//*Just to keep the track of the jQuery edits vs JS
-
-document.getElementById("loan-form").addEventListener("submit", calculateResults);
-document.getElementById("loan-form").addEventListener("reset", hideOutputs);
-// ** First Try **
-// function reset() {
-//     document.querySelectorAll(".title").innerHTML = 0;
-//     console.log("reset ran");
-// }
 
 
-// ** Second Try **
-    // if (elements.display === "block") {
-    //     elements.display = "none";
-    // } else {
-    //     elements.display = "block";
-    // }
-    
-    // elements.forEach((el) => {
-    //     if (elements.display === "block") {
-    //         el.classList.add("show");
-    //     } else {
-    //         el.classList.add("hide");
-    //     }
-    //   });
 
+document.getElementById("loan-form").addEventListener("submit", (e) => {
+    const carPrice = document.getElementById("car-price").value;
+    e.preventDefault(); // To make sure the page doesn't reload when the submit button is pushed
+    if (carPrice != "") {
+        calculateResults();
+    }
+});
 
-// ** Third Try **
+document.getElementById("loan-form").addEventListener("reset", resetOutputs);
 
-// function reset() {
-//     const outputs = document.querySelectorAll(".is-1");
-//     outputs.classList.add("hide");
-// }
-
-function calculateResults(e) {
+function calculateResults() {
 
     // Linking to inputs
 
@@ -82,22 +45,67 @@ function calculateResults(e) {
     document.getElementById("monthlyPayment").innerHTML = "$" + formatter.format(monthlyPayment);
     document.getElementById("totalInterest").innerHTML = "$" + formatter.format(totalInterest);
     document.getElementById("totalAmount").innerHTML = "$" + formatter.format(totalPayment);
-
-    e.preventDefault(); // To make sure the page doesn't reload when the submit button is pushed
-
-        // Removing the hide class and showing the results
-
-        const elements = document.querySelectorAll(".title");
-        elements.forEach((el) => {
-        el.classList.remove("hide");
-      });
 }
 
 
-function hideOutputs() {
+function resetOutputs() {
     const elements = document.querySelectorAll(".title");
     elements.forEach((el) => {
-        el.classList.add("hide");
-      });
+        el.textContent = "";
+    });
 }
-hideOutputs();
+
+
+
+
+
+
+
+// waits for the DOM to load
+
+    
+//    let $submitButton = document.getElementById("submit-btn")
+
+//    $submitButton.addEventListener("click", function(e){
+//     e.preventDefault()
+//     let salesTax = $('#sales-tax').val()
+//     let downPayment = $('#down-payment').val()    
+//     let tradeIn = $('#trade-value').val()    
+//     let loanTerm = $('#loan-term').val()    
+//     let interestRate = $('#interest-rate').val()    
+
+//     console.log(salesTax, downPayment, tradeIn, loanTerm, interestRate)
+// })
+
+//*Just to keep the track of the jQuery edits vs JS
+
+
+// ** First Try **
+// function reset() {
+//     document.querySelectorAll(".title").innerHTML = 0;
+//     console.log("reset ran");
+// }
+
+
+// ** Second Try **
+    // if (elements.display === "block") {
+    //     elements.display = "none";
+    // } else {
+    //     elements.display = "block";
+    // }
+    
+    // elements.forEach((el) => {
+    //     if (elements.display === "block") {
+    //         el.classList.add("show");
+    //     } else {
+    //         el.classList.add("hide");
+    //     }
+    //   });
+
+
+// ** Third Try **
+
+// function reset() {
+//     const outputs = document.querySelectorAll(".is-1");
+//     outputs.classList.add("hide");
+// }
